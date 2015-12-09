ce21a543f0730b0456218e5f143e939407db0721
lambertmeyer <meyer.lambert68@gmail.com>
2015-12-09 11:56:04 +0100

    nav

--- apps/content/subcategory.php
+++ apps/content/subcategory.php
@@ -1,6 +1,9 @@
 <?php
-$subCategoryManager = new SubcategoryManager($database);
-$subCategory = $subCategoryManager->findById($_GET['id']);
-require('views/content/subcategory.phtml');
-
+if(isset($_GET['id']))
+{
+	$subCategoryManager = new SubcategoryManager($database);
+	$subCategory = $subCategoryManager->findById($_GET['id']);
+	require('views/content/subcategory.phtml');
+	
+}
 ?>
