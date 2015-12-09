a3b80d2b922e8c6429ac9261be8f4723694960d4
lambertmeyer <meyer.lambert68@gmail.com>
2015-12-09 10:02:12 +0100

    nav

--- apps/nav.php
+++ apps/nav.php
@@ -2,6 +2,16 @@
 <?php
 	if (isset($_SESSION["id"])) 
 	{
+		if ($currentUser->getRights() == 2)
+		{
+			$url = 'admin';
+			$name = 'Administration';
+		}
+		else
+		{
+			$url = 'cart';
+			$name = 'Votre panier';
+		}
 		require('views/nav_in.phtml');
 	}
 	else
